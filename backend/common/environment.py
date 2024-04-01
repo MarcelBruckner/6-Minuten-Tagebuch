03da1f1f94ca682d6f4f57c377b8432534bb566e
Marcel Bruckner <bruckner@cqse.eu>
2024-04-01 17:29:09 +0200

    Switch to .env file

--- backend/common/environment.py
+++ backend/common/environment.py
@@ -7,8 +7,8 @@ def get_data_path():
 
 
 def get_access_token_expires_minutes():
-    return int(os.environ.get('ACCESS_TOKEN_EXPIRE_MINUTES', 60*24))
+    return int(os.environ.get('ACCESS_TOKEN_EXPIRE_MINUTES', 1440))
 
 
 def get_app_secret_key():
-    return os.environ.get('APP_SECRET_KEY', '4352c9036368100cb0e7292aae9c0d10eee244b147a1a53148c2cfd19ecbd324')
+    return os.environ.get('APP_SECRET_KEY', 'YF3FUdiO7NZkcpzcxi69kFP7XHdyLPclf+BnMJAO9Kg=')
